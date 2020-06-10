0595c8d47bf7c1a5fa85ddf430c672846f2e4d70
Dean Erickson <dmepilot@yahoo.com>
2020-06-10 18:00:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-first_hash = {"test_key" => widgets, "made_up_stuff" => nanooka, "more stuff" => chewie} 
+first_hash = {"test_key" => "widgets", "made_up_stuff" => "nanooka", "more stuff" => "chewie"} 
   
 end
 
